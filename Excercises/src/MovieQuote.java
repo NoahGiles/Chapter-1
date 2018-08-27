352301d8674f3b4ba65752b6bb52ce0176d5d4ee
NoahGiles <billywerber@gmail.com>
2018-08-27 09:50:47 -0500

    Updated text

---
+++ Excercises/src/MovieQuote.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class MovieQuote {
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "I've got a feeling we're not in Kansas anymore");
+
+	}
+
+}
--- Excercises/src/TableAndChairs.java
+++ Excercises/src/TableAndChairs.java
@@ -3,7 +3,31 @@ public class TableAndChairs {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hello World!");
+		
+		
+		System.out.println("X                           X");
+		System.out.println("X                           X");
+		System.out.println("X                           X");
+		System.out.println("X       XXXXXXXXXXXXX       X");
+		System.out.println("XXXX    X           X    XXXX");
+		System.out.println("X  X    X           X    X  X");
+		
+		displayTruck();
 	}
-
+	
+	public static void displayTruck() {
+		System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXX    ");
+		System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXX   ");
+		System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXX  ");
+		System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXX ");
+		System.out.println("XXXXXX      X      XXX    X  ");
+		System.out.println("XXXXXX      X      XXX    XX ");
+		System.out.println("XXXXXXXXXXXXXXXXXXXXXX    XXX");
+		System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+		System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+		System.out.println("  XXX                 XXX    ");
+		
+		
+	}
 }
