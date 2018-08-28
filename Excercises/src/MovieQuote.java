ee609b82a65a6daaaead628a2dd570770efefea1
NoahGiles <billywerber@gmail.com>
2018-08-28 08:47:08 -0500

    Movie quote updated

--- Excercises/src/MovieQuote.java
+++ Excercises/src/MovieQuote.java
@@ -4,7 +4,7 @@ public class MovieQuote {
 		
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		JOptionPane.showMessageDialog(null, "I've got a feeling we're not in Kansas anymore");
+		JOptionPane.showMessageDialog(null, "Ya like jazz?");
 
 	}
 
