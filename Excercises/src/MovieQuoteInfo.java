9610c12c92cfc3a1997de9aaf058d60ec569a98a
NoahGiles <billywerber@gmail.com>
2018-08-28 08:45:50 -0500

    Updated movie quote

--- Excercises/src/MovieQuoteInfo.java
+++ Excercises/src/MovieQuoteInfo.java
@@ -1,9 +1,10 @@
+import javax.swing.JOptionPane;
 
 public class MovieQuoteInfo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		JOptionPane.showMessageDialog(null, "Barry B. Benson, The Bee Movie, 2007");
 	}
 
 }
