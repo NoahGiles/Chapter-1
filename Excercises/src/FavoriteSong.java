d4cb670598393a069ce2745f194c0e524bc28e38
NoahGiles <billywerber@gmail.com>
2018-08-28 08:55:13 -0500

    Updated Favorite Song

--- Excercises/src/FavoriteSong.java
+++ Excercises/src/FavoriteSong.java
@@ -1,9 +1,13 @@
+import javax.swing.JOptionPane;
 
 public class FavoriteSong {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		JOptionPane.showMessageDialog(null, "Didn't know what time it was the lights were low");
+		JOptionPane.showMessageDialog(null, "I leaned back on my radio");
+		JOptionPane.showMessageDialog(null, "Some cat was layin' down some rock 'n' roll");
+		JOptionPane.showMessageDialog(null, "Then the loud sound did seem to fade");
 	}
 
 }
