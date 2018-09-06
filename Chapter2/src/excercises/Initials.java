d969e671964dd7fe12610577529bbcd2c135c86c
NoahGiles <billywerber@gmail.com>
2018-09-06 08:53:01 -0500

    Debugging

--- Chapter2/src/excercises/Initials.java
+++ Chapter2/src/excercises/Initials.java
@@ -1,15 +1,14 @@
 package excercises;
 
-import java.util.Scanner;
 
 public class Initials {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String FirstInitial = "J";
-		String SecondInitial = "M";
-		String ThirdInitial = "F";
+		char FirstInitial = 'J';
+		char SecondInitial = 'M';
+		char ThirdInitial = 'F';
 		
 		System.out.println("Your initials are " + FirstInitial + "." + SecondInitial + "." + ThirdInitial
 				+ ".");
---
+++ Chapter2/src/gameZone/MadLib.java
@@ -0,0 +1,11 @@
+package gameZone;
+
+public class MadLib {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	}
+
+}
